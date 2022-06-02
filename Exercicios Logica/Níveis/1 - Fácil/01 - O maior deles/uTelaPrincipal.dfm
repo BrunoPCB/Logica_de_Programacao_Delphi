a7f10dd9c45deb44a0a9eb952e15d1ac6e48e586
BrunoPCB <48020942+BrunoPCB@users.noreply.github.com>
2022-06-02 11:24:54 -0300

    Atualizando codigo

--- Exercicios Logica/Níveis/1 - Fácil/01 - O maior deles/uTelaPrincipal.dfm
+++ Exercicios Logica/Níveis/1 - Fácil/01 - O maior deles/uTelaPrincipal.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 231
-  ClientWidth = 678
+  ClientHeight = 262
+  ClientWidth = 385
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,7 +11,38 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
-  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object lblResultado: TLabel
+    Left = 88
+    Top = 208
+    Width = 178
+    Height = 13
+    Caption = 'O n'#250'mero  X  '#233' maior que o n'#250'mero Y'
+  end
+  object edNumero1: TEdit
+    Left = 120
+    Top = 40
+    Width = 129
+    Height = 21
+    TabOrder = 0
+    TextHint = 'N'#250'mero 1'
+  end
+  object edNumero2: TEdit
+    Left = 120
+    Top = 96
+    Width = 129
+    Height = 21
+    TabOrder = 1
+    TextHint = 'N'#250'mero 2'
+  end
+  object btnMaior: TButton
+    Left = 144
+    Top = 136
+    Width = 75
+    Height = 25
+    Caption = 'Maior'
+    TabOrder = 2
+    OnClick = btnMaiorClick
+  end
 end
--- Exercicios Logica/Níveis/1 - Fácil/01 - O maior deles/uTelaPrincipal.pas
+++ Exercicios Logica/Níveis/1 - Fácil/01 - O maior deles/uTelaPrincipal.pas
@@ -4,11 +4,15 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
-    procedure FormCreate(Sender: TObject);
+    edNumero1: TEdit;
+    edNumero2: TEdit;
+    btnMaior: TButton;
+    lblResultado: TLabel;
+    procedure btnMaiorClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -22,7 +26,7 @@ implementation
 
 {$R *.dfm}
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.btnMaiorClick(Sender: TObject);
 Var
   Num1, Num2: Integer;
 begin
@@ -31,16 +35,23 @@ begin
   // Faça um programa que peça dois números e verifique
   // (usando if e else) e imprima o maior deles
 
-  Num1 := 0;
-  Num2 := 0;
-
-  if Num1 > Num2 then
+  if (edNumero1.Text = '') or (edNumero2.Text = '') then
   begin
-    ShowMessage('O número ' + IntToStr(Num1) + ' é maior que o número ' + IntToStr(Num2));
+    ShowMessage('Informe um valor para cada campo')
   end
   else
-    ShowMessage('O número ' + IntToStr(Num2) + ' é maior que o número ' + IntToStr(Num1));
+  begin
 
+    Num1 := StrToInt(edNumero1.Text);
+    Num2 := StrToInt(edNumero2.Text);
+
+    if Num1 > Num2 then
+    begin
+      lblResultado.Caption := 'O número ' + IntToStr(Num1) + ' é maior que o número ' + IntToStr(Num2);
+    end
+    else
+      lblResultado.Caption := 'O número ' + IntToStr(Num2) + ' é maior que o número ' + IntToStr(Num1);
+  end;
 
 end;
 
